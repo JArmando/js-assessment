67da69518d5c1fa49f2a1c8d218ff808d6ae63bc
JArmando <JArmando@racowireless.com>
2014-08-14 16:57:24 -0400

    done with objects

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,21 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+    	return fn.call(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+    	return constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+    	var o = [];
+    	for(a in obj){
+    		if(obj.hasOwnProperty(a)){
+    			o.push(a +': '+obj[a]);
+    		}
+    	}
+    	return o;
     }
   };
 });
